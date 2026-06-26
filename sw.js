35e24da5f931548382426719c88fadbdbbece77b
liviuradu <115651143+liviuradu@users.noreply.github.com>
2026-06-26 15:04:30 +0200

    Geraete-Liste (298 Maschinen) + Materialien

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "btb-v3";
+const CACHE = "btb-v4";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
