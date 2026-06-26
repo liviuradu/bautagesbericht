d5fccf59d9f2634f76fbf30b5c3060497c6fbded
liviuradu <115651143+liviuradu@users.noreply.github.com>
2026-06-26 03:29:24 +0200

    Hide LV-Leistung line; bump SW to btb-v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "btb-v2";
+const CACHE = "btb-v3";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
